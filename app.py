84b1d0cd8a54c3f3ce1598258989da319828c7ea
Abdelhamid1985 <boss.abdelhamid@gmail.com>
2021-08-15 15:21:12 -0400

    Grabing languages from repos and working on the view

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 def home():
     GhitRepos = requests.get("https://api.github.com/search/repositories?q=created:%3E2021-08-01&sort=stars&order=desc&page=1&&per_page=100").json()
     return render_template('layout.html',
-        heading='Sample section',
-        body_text='Very important<br/>message here!',
+        heading='GhitHub Languages Statistics',
+        body_text='Find out a list of most used languages for top 100 trending repositories<br/><br /><hr /><br /><br />',
         pages=[
-            dict(title='Home', href='/'),
-            dict(title='About', href='/about')
+            dict(title='Home', href='/'), # List infos about the app
+            dict(title='Stats', href='/stats') # Provide statistics for languages
         ],
-        GhitRepos = GhitRepos
+        items = GhitRepos['items']
     )
